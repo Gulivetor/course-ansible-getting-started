c01d92c3b87360527dceb3e19fbb7439ccd262dd
AJR <adamjr1083@yahoo.com>
2021-07-07 17:45:43 +0300

    edited newuser script

--- inventory/vagrants/newuser.sh
+++ inventory/vagrants/newuser.sh
@@ -1,17 +1,23 @@
 user=tux
 pass=tuxmuxB@1
 
-#sudo cat /etc/ssh/sshd_config | grep -i PasswordAuthentication
+sudo cat /etc/ssh/sshd_config | grep -i PasswordAuthentication
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 
-#sudo cat /etc/ssh/sshd_config | grep -i PubkeyAuthentication
+sudo cat /etc/ssh/sshd_config | grep -i PubkeyAuthentication
 sudo sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
 
-sudo useradd $user
-
-echo -e "$pass\n$pass" | sudo /usr/bin/passwd $user
-
-#sudo usermod -aG sudo $user
-sudo usermod -aG wheel $user
+if grep "CentOS Linux 8" /etc/os-release
+then
+    sudo useradd $user
+    echo -e "$pass\n$pass" | sudo /usr/bin/passwd $user
+    sudo usermod -aG wheel $user
+elif grep "Ubuntu 18.04.5 LTS" /etc/os-release
+then
+    echo -e "$pass\n$pass" | sudo adduser tux -q
+    sudo usermod -aG sudo $user
+else
+    echo Nope
+fi
 
 sudo systemctl restart sshd.service
